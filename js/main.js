a9f71bf9e3a90bb2809a02aacead0fd8e647023d
akki1027 <akki10271995@gmail.com>
2020-07-30 21:46:57 +0900

    To-do list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
 (function () {
-  'use strict'
+  'use strict';
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      name: 'aki'
+    }
+  });
+})();
+
+(function () {
+  'use strict';
+
+  var td = new Vue({
+    el: '#to-do',
+    data: {
+      todos: [{
+        title: 'task 1',
+        isDone: false
+      }, {
+        title: 'task 2',
+        isDone: false
+      }, {
+        title: 'task 3',
+        isDone: true
+      }]
+    },
+    methods: {
+      addItem: function () {
+        var item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function (index) {
+        if (confirm('are you sure?')) {
+          this.todos.splice(index, 1);
+        }
+      }
+    },
+    computed: {
+      remaining: function () {
+        var items = this.todos.filter(function (todo) {
+          return !todo.isDone;
+        });
+        return items.length;
+      }
+    }
+  });
 })();
